d14076b13c6b81956b9479a8cf96c685d52b8e83
CaioLima <caiohenrys50@gmail.com>
2026-07-31 18:15:46 -0300

    create deliveries

--- src/controllers/deliveries-controller.ts
+++ src/controllers/deliveries-controller.ts
@@ -1,8 +1,25 @@
+import { prisma } from "@/database/prisma"
 import { Request, Response, NextFunction } from "express"
+import z from "zod"
 
 class DeliveriesController {
-  create(request: Request, response: Response, next: NextFunction){
-    return response.json({ message: "okk" })
+  async create(request: Request, response: Response, next: NextFunction){
+    
+    const bodySchema = z.object({
+      user_id: z.string().uuid(),
+      description: z.string()
+    })
+
+    const { user_id, description } = bodySchema.parse(request.body)
+
+    await prisma.delivery.create({
+      data: {
+        userId: user_id,
+        description
+      }
+    })
+    
+    return response.status(201).json()
   }
 }
 
