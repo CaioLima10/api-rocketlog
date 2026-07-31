bbe1cbb61a4677e7149844c79afd416d3f9363b6
CaioLima <caiohenrys50@gmail.com>
2026-07-31 03:27:33 -0300

    create user

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { routes } from "./routes"
 const app = express()
 
 app.use(express.json())
-app.use(errorHandling)
 app.use(routes)
+app.use(errorHandling)
 
 export { app }
--- src/middlewares/error-handling.ts
+++ src/middlewares/error-handling.ts
@@ -8,16 +8,16 @@ function errorHandling(
   response: Response,
   _next: NextFunction
 ){
+
+  
   if(error instanceof AppError) {
     return response.status(error.statusCode).json({ message: error.message })
   }
-
+  
   if(error instanceof ZodError) {
-    return response.status(400).json({
-      message: "Validation error",
-      issues: error.format()
-    })
+    return response.status(400).json({ message: "Validation error", issues: error.format()})
   }
+
 
   return response.status(500).json({ message: error.message })
 }
