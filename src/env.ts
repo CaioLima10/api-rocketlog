2eea412cfd7ee064774f111f84a690704ecb474b
CaioLima <caiohenrys50@gmail.com>
2026-08-10 01:10:23 -0300

    config port

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,8 @@ import z from "zod";
 
 const envShema = z.object({
   DATABASE_URL: z.string().url(),
-  JWT_SECRET: z.string()
+  JWT_SECRET: z.string(),
+  PORT: z.coerce.number().default(3456)
 })
 
 export const env = envShema.parse(process.env)
